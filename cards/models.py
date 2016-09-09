149b7d8051be360e703a03bb74b3d6a4f1e4a833
Thomas (Junrui) Hu <junruihu@gmail.com>
2016-09-09 17:13:36 +0900

    make temporary models

--- cards/models.py
+++ cards/models.py
@@ -24,3 +24,42 @@ class Card(models.Model):
     def get_absolute_url(self):
         return reverse('cards:detail', kwargs={'pk': self.pk})
 
+
+class Card2(models.Model):
+    user = models.ForeignKey(User, default=1)
+
+    topic = models.CharField(max_length=100, verbose_name='Song Title')
+    front = models.CharField(max_length=100, verbose_name='Artist Name')
+    back = models.CharField(max_length=10000, verbose_name='Lyrics & Tabs')
+
+    review_time = models.DateTimeField()
+    review_interval = models.BigIntegerField(default=60)    # in seconds
+
+    card_audio = models.CharField(default='', max_length=300, verbose_name='Youtube Link')
+    card_score = models.CharField(default='', max_length=300, verbose_name='Lyrics Link')
+    card_pic = models.FileField(verbose_name='Cover Picture')
+
+    is_new = models.BooleanField(default=True)
+
+    def get_absolute_url(self):
+        return reverse('cards:detail', kwargs={'pk': self.pk})
+
+
+class Card3(models.Model):
+    user = models.ForeignKey(User, default=1)
+
+    topic = models.CharField(max_length=100, verbose_name='Song Title')
+    front = models.CharField(max_length=100, verbose_name='Artist Name')
+    back = models.CharField(max_length=10000, verbose_name='Lyrics & Tabs')
+
+    review_time = models.DateTimeField()
+    review_interval = models.BigIntegerField(default=60)    # in seconds
+
+    card_audio = models.CharField(default='', max_length=300, verbose_name='Youtube Link')
+    card_score = models.CharField(default='', max_length=300, verbose_name='Lyrics Link')
+    card_pic = models.FileField(verbose_name='Cover Picture')
+
+    is_new = models.BooleanField(default=True)
+
+    def get_absolute_url(self):
+        return reverse('cards:detail', kwargs={'pk': self.pk})
